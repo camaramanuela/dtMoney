70d86f5bdbdd6847cc3a91dcd0ad710900fac3e7
Manuela Bognar <manuelabcamara@gmail.com>
2021-07-17 09:23:14 -0300

    fonts

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ export function App() {
   return (
     <div className="App">
       <h1>Hello World but now with Styled components =]</h1>
+      <p>This is a paragraph.</p>
       <GlobalStyle />
     </div>
   );
